6ef852f861bb86f3cf75033dca3229209055cfcf
Haivher <164926769+Haivher@users.noreply.github.com>
2024-06-15 21:43:13 -0400

    Add files via upload

---
+++ validacion.js
@@ -0,0 +1,32 @@
+var nom = document.getElementById('floatingInput');
+var password = document.getElementById('floatingPassword');
+var error = document.getElementById('error');
+error.style.color = 'red';
+
+var form = document.getElementById('formulario');
+form.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+    var mensajesError = [];
+
+    if (nom.value === null || nom.value === 'haivher' && password.value === null || password.value === '123') {
+        window.location = 'index2.html';
+    }
+    if (nom.value === null || nom.value === '') {
+        mensajesError.push('Ingresa tu Usuario');
+    }
+    if (password.value === null || password.value === '') {
+        mensajesError.push('Ingresa tu Contraseña');
+    }
+
+    error.innerHTML = mensajesError.join(' , ');
+
+    return false;
+});
+
+function infor() {
+    if ( nom.value != 'haivher' && password.value != '123' && nom.value != ''&& password.value != '') {
+        alert("Datos Incorrectos, Intente Nuevamente");
+        return false,
+            window.location = 'index.html';
+    }
+}
